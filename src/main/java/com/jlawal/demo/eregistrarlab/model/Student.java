de670512409143221ce521bbbeec973b2a6ea848
John Lawal <jilawal@mum.edu>
2019-08-02 15:02:35 -0500

    created folders, setting up project

---
+++ src/main/java/com/jlawal/demo/eregistrarlab/model/Student.java
@@ -0,0 +1,187 @@
+package com.jlawal.demo.eregistrarlab.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "students")
+public class Student {
+//
+//	   studentNumber: e.g. 000-61-0001 (required)
+//
+//	   FirstName: e.g. Anna (required)
+//
+//	   MiddleName: (optional)
+//
+//	   LastName: e.g. Smith (required)
+//
+//	   cgpa: e.g. 3.78 (optional)
+//
+//	   enrollmentDate: e.g. 2019-5-12 (required)
+//
+//	   isInternational: (values: "Yes", "No") (required) - Hint: Implement/present this data in a drop-down list
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long studentId;
+	
+	@NotBlank(message = "Please enter the Student's Number")
+	@Column(nullable = false)
+	private String studentNumber;
+	
+	@NotBlank(message = "Please enter the Student's First Name")
+	@Column(nullable = false)
+	private String firstName;
+	
+	private String middleName;
+	
+	@NotBlank(message = "Please enter the Student's Last Name")
+	@Column(nullable = false)
+	private String lastName;
+	
+	private Double cgpa;
+	
+	@NotBlank(message = "Please enter the Enrollment Date")
+	@Column(nullable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate enrollmentDate;
+	
+	@NotBlank(message = "Please specify the Nationality")
+	@Column(nullable = false)
+	private Boolean isInternational;
+
+	public Student(){
+		
+	}
+	
+	public Student(String studentNumber,
+			 String firstName, String middleName,
+			 String lastName, Double cgpa,
+			 LocalDate enrollmentDate,
+			 Boolean isInternational) {
+		super();
+		this.studentNumber = studentNumber;
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.cgpa = cgpa;
+		this.enrollmentDate = enrollmentDate;
+		this.isInternational = isInternational;
+	}
+
+	
+	public Long getStudentId() {
+		return studentId;
+	}
+
+
+
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
+
+
+
+	public String getStudentNumber() {
+		return studentNumber;
+	}
+
+
+
+	public void setStudentNumber(String studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public Double getCgpa() {
+		return cgpa;
+	}
+
+
+
+	public void setCgpa(Double cgpa) {
+		this.cgpa = cgpa;
+	}
+
+
+
+	public LocalDate getEnrollmentDate() {
+		return enrollmentDate;
+	}
+
+
+
+	public void setEnrollmentDate(LocalDate enrollmentDate) {
+		this.enrollmentDate = enrollmentDate;
+	}
+
+
+
+	public Boolean getIsInternational() {
+		return isInternational;
+	}
+
+
+
+	public void setIsInternational(Boolean isInternational) {
+		this.isInternational = isInternational;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentNumber=" + studentNumber + ", firstName=" + firstName
+				+ ", middleName=" + middleName + ", lastName=" + lastName + ", cgpa=" + cgpa + ", enrollmentDate="
+				+ enrollmentDate + ", isInternational=" + isInternational + "]";
+	}
+	
+	
+
+}
