0272657875d63bb6ddd9519f6efec73f06bd8fbd
John Lawal <jilawal@mum.edu>
2019-08-02 14:15:07 -0500

    first cut, setting up project

---
+++ src/main/java/com/jlawal/demo/eregistrarlab/ERegistrarLabApplication.java
@@ -0,0 +1,13 @@
+package com.jlawal.demo.eregistrarlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERegistrarLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERegistrarLabApplication.class, args);
+	}
+
+}
